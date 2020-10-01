ebdf3068079c5c9102a08311b18646be996c1d76
Denys <pugachovdenys@gmail.com>
2020-10-01 23:09:34 +0300

    use Transition (react-trasition-group)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Transition } from 'react-transition-group';
 
 function App() {
+    const [toggle, setToggle] = useState(true);
     return (
         <div className="container">
-
-        </div>
+            <button onClick={ () => setToggle(!toggle) }> Toggle</button>
+            <hr />
+            <div className={ "blocks" }>
+                <Transition
+                    in={ toggle }
+                    timeout={ {
+                        enter: 1000,
+                        exit: 500,
+                    } }
+                    mountOnEnter
+                    unmountOnExit
+                >
+                    { state => <div className={ `square blue ${state}` }>{ state }</div> }
+                </Transition>
+            </div>
+        </div >
     );
-}
+};
 
 export default App;
